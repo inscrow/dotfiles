1b28862601c86d4668c8e82f8cb4febcbebb0d41
inscrow <rickyrie98ma@gmail.com>
2022-04-16 16:23:12 +0200

    mpv tagged

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -39,6 +39,7 @@ static const Rule rules[] = {
 	/* class      instance    title      tags mask     isfloating   isterminal  noswallow  monitor */
 	{ "qutebrowser", NULL,    NULL,      1 << 0,         0,         0,           0,          -1 },
 	{ "Gimp",        NULL,    NULL,      1 << 8,         1,         0,           0,          -1 },
+	{ "mpv",         NULL,    NULL,      1 << 3,         0,         0,           0,          -1 },
 	{ "St",          NULL,    NULL,         0,           0,         1,           0,          -1 },
 	{ "st-256color", NULL,    NULL,         0,           0,         1,           0,          -1 },
 };
