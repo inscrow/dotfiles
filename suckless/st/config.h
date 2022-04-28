6e411dadce6f8c1847d917ddb0685451697f7174
inscrow <rickyrie98ma@gmail.com>
2022-04-28 21:53:12 +0200

    monochrome terminal theme

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -99,24 +99,29 @@ float alpha = 1.0;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	[0] = "#f3ece7", /* hard contrast: #f9f5d7 / soft contrast: #f2e5bc */
-	[1] = "#cc241d", /* red     */
-	[2] = "#98971a", /* green   */
-	[3] = "#d79921", /* yellow  */
-	[4] = "#458588", /* blue    */
-	[5] = "#b16286", /* magenta */
-	[6] = "#689d6a", /* cyan    */
-	[7] = "#7c6f64", /* white   */
+	"#000000",
+	"#151515",
+	"#303030",
+	"#454545",
+	"#606060",
+	"#757575",
+	"#909090",
+	"#F9F9F9",
 
 	/* 8 bright colors */
-	[8]  = "#928374", /* black   */
-	[9]  = "#9d0006", /* red     */
-	[10] = "#79740e", /* green   */
-	[11] = "#b57614", /* yellow  */
-	[12] = "#076678", /* blue    */
-	[13] = "#8f3f71", /* magenta */
-	[14] = "#427b58", /* cyan    */
-	[15] = "#000000", /* white   */
+	"#000000",
+	"#151515",
+	"#303030",
+	"#454545",
+	"#606060",
+	"#757575",
+	"#909090",
+	"#F9F9F9",
+	
+	[255] = 0,
+	/* special colors */
+	[256] = "#f3ece7", /* background */
+	[257] = "#000000", /* foreground */
 };
 
 
@@ -124,10 +129,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-unsigned int defaultcs = 15;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
 
 /*
  * https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Ps-SP-q.1D81
