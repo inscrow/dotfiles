6f150f9ce3bdc8443c7787ca43f2762e8b805d5f
inscrow <rickyrie98ma@gmail.com>
2022-05-22 19:07:07 +0200

    adjusted browser and startpage colors

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -8,7 +8,7 @@ c.aliases = {
 }
 
 # don't show scrollbars
-c.scrolling.bar = 'never'
+c.scrolling.bar = "never"
 
 c.content.geolocation = False
 
@@ -44,37 +44,44 @@ config.bind(';f', 'hint links tab-bg')
 config.bind('<Ctrl-l>', 'search')
 
 # colors
-black     = "#000000"
+c.window.transparent = True # enable transparency
+
+white     = "#ffffff"
 magenta   = "#b16286"
 green     = "#98971a"
-highlight = "#bd856a"
+highlight = "#002b36"
 red       = "#cc241d"
-white     = "#f3ece7"
+black     = "#000000"
 yellow    = "#d79921"
 
-# c.colors.webpage.darkmode.enabled = True
+alpha = 0.55
+alpha = "" + hex(int(alpha * 0xff))
+transBG = "#" + alpha[2:] + "000000"
+
+# c.colors.webpage.bg = transBG
+c.colors.webpage.darkmode.enabled = True
 # c.colors.webpage.darkmode.algorithm = "lightness-hsl"
 # c.colors.webpage.darkmode.threshold.text = 150
 # c.colors.webpage.darkmode.threshold.background = 205
-# c.colors.webpage.darkmode.policy.images = "never"
-# c.colors.webpage.darkmode.policy.page = "smart"
-# c.colors.webpage.preferred_color_scheme = "dark"
+c.colors.webpage.darkmode.policy.images = "never"
+c.colors.webpage.darkmode.policy.page = "smart"
+c.colors.webpage.preferred_color_scheme = "dark"
 
 # completion menu
 c.colors.completion.category.bg = magenta
 c.colors.completion.category.border.bottom = magenta
 c.colors.completion.category.border.top = magenta
-c.colors.completion.category.fg = white
+c.colors.completion.category.fg = black
 
-c.colors.completion.even.bg = white
-c.colors.completion.odd.bg = white
-c.colors.completion.fg = black
+c.colors.completion.even.bg = black
+c.colors.completion.odd.bg = black
+c.colors.completion.fg = white
 
 c.colors.completion.item.selected.bg = yellow
 c.colors.completion.item.selected.border.bottom = yellow
 c.colors.completion.item.selected.border.top = yellow
-c.colors.completion.item.selected.fg = black
-c.colors.completion.item.selected.match.fg = white
+c.colors.completion.item.selected.fg = white
+c.colors.completion.item.selected.match.fg = black
 c.colors.completion.match.fg = yellow
 
 c.completion.scrollbar.width = 0
@@ -85,71 +92,71 @@ c.completion.shrink = True
 c.completion.use_best_match = True
 
 # context menu
-c.colors.contextmenu.disabled.bg = white
+c.colors.contextmenu.disabled.bg = black
 c.colors.contextmenu.disabled.fg = highlight
-c.colors.contextmenu.menu.bg = white
-c.colors.contextmenu.menu.fg = black
+c.colors.contextmenu.menu.bg = black
+c.colors.contextmenu.menu.fg = white
 c.colors.contextmenu.selected.bg = yellow
-c.colors.contextmenu.selected.fg = black
+c.colors.contextmenu.selected.fg = white
 
 # downloads bar
-c.colors.downloads.bar.bg = white
+c.colors.downloads.bar.bg = black
 c.colors.downloads.error.bg = red
-c.colors.downloads.error.fg = white
+c.colors.downloads.error.fg = black
 c.colors.downloads.start.bg = highlight
-c.colors.downloads.start.fg = black
+c.colors.downloads.start.fg = white
 c.colors.downloads.stop.bg = green
-c.colors.downloads.stop.fg = black
+c.colors.downloads.stop.fg = white
 
 # hints
 c.colors.hints.bg = yellow
-c.colors.hints.fg = white
-c.colors.hints.match.fg = black
+c.colors.hints.fg = black
+c.colors.hints.match.fg = white
 
 # key hints
-c.colors.keyhint.bg = black
-c.colors.keyhint.fg = white
+c.colors.keyhint.bg = white
+c.colors.keyhint.fg = black
 c.colors.keyhint.suffix.fg = yellow
 
 # messages
 c.colors.messages.error.bg = yellow
 c.colors.messages.error.border = yellow
-c.colors.messages.error.fg = black
+c.colors.messages.error.fg = white
 
-c.colors.messages.info.bg = white
-c.colors.messages.info.border = white
-c.colors.messages.info.fg = black
+c.colors.messages.info.bg = black
+c.colors.messages.info.border = black
+c.colors.messages.info.fg = white
 
-c.colors.messages.warning.bg = white
+c.colors.messages.warning.bg = black
 c.colors.messages.warning.border = yellow
-c.colors.messages.warning.fg = black
+c.colors.messages.warning.fg = white
 
 # prompt
-c.colors.prompts.bg = white
-c.colors.prompts.border = black
-c.colors.prompts.fg = black
+c.colors.prompts.bg = black
+c.colors.prompts.border = white
+c.colors.prompts.fg = white
 
 c.colors.prompts.selected.bg = yellow
-c.colors.prompts.selected.fg = black
+c.colors.prompts.selected.fg = white
 
 # statusbar
 c.colors.statusbar.caret.bg = yellow
-c.colors.statusbar.caret.fg = black
+c.colors.statusbar.caret.fg = white
 c.colors.statusbar.caret.selection.bg = yellow
-c.colors.statusbar.caret.selection.fg = white
+c.colors.statusbar.caret.selection.fg = black
 c.colors.statusbar.command.bg = highlight
-c.colors.statusbar.command.fg = black
+c.colors.statusbar.command.fg = white
 c.colors.statusbar.command.private.bg = yellow
-c.colors.statusbar.command.private.fg = black
+c.colors.statusbar.command.private.fg = white
 c.colors.statusbar.insert.bg = magenta
-c.colors.statusbar.insert.fg = white
-c.colors.statusbar.normal.bg = white
-c.colors.statusbar.normal.fg = black
+c.colors.statusbar.insert.fg = black
+c.colors.statusbar.normal.bg = transBG
+c.colors.statusbar.normal.fg = white
 c.colors.statusbar.url.error.fg = red
-c.colors.statusbar.url.fg = black
+c.colors.statusbar.url.fg = white
 c.colors.statusbar.url.hover.fg = magenta
 
-c.colors.statusbar.progress.bg = black
+c.colors.statusbar.progress.bg = white
 c.colors.statusbar.url.success.http.fg = green
 c.colors.statusbar.url.success.https.fg = green
 c.colors.statusbar.url.hover.fg = magenta
@@ -159,19 +166,20 @@ c.statusbar.show = "always"
 c.statusbar.widgets = ['progress', 'keypress', 'url', 'history', 'scroll']
 
 # tabs
-c.colors.tabs.bar.bg = white
-c.colors.tabs.even.bg = white
-c.colors.tabs.even.fg = black
-c.colors.tabs.odd.bg = white
-c.colors.tabs.odd.fg = black
+c.colors.tabs.bar.bg = transBG
+c.colors.tabs.even.bg = transBG
+c.colors.tabs.even.fg = white
+c.colors.tabs.odd.bg = transBG
+c.colors.tabs.odd.fg = white
 
 c.colors.tabs.selected.even.bg = highlight
-c.colors.tabs.selected.even.fg = black
+c.colors.tabs.selected.even.fg = white
 c.colors.tabs.selected.odd.bg = highlight
-c.colors.tabs.selected.odd.fg = black
+c.colors.tabs.selected.odd.fg = white
 
 c.tabs.position = "bottom"
 c.tabs.show = "multiple"
+
 
 # startpage
 c.url.start_pages = '/home/rick/.config/startpage/index.html'
